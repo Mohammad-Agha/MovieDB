1a356d00bb143f20b9cb526c94937f4a6b095307
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-21 16:49:26 +0300

    Created an express simple API

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ const express = require('express')
 // Execute express module
 const app = express()
 
-// Creating our route to home
+// Creating route to home
 app.get('/', (req, res) => {
-    res.send('Ok')
+  res.send('Ok')
+})
+
+// Creating route to test
+app.get('/test', (req, res) => {
+  const result = {
+    status: 200,
+    message: "Ok"
+  }
+  res.send(result)
+})
+
+// Creating route to time
+app.get('/time', (req, res) => {
+  const time = new Date()
+  const timeString = `${time.getHours()}:${time.getMinutes()}`
+  const result = {
+    status: 200,
+    message: timeString
+  }
+  res.send(result);
 })
 
 // We start listening to the server on port 3000
